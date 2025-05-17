2ee1d8c40ea2e4bddcc0fc6a6fd4db9ae4e46f9c
shihow dmitri <shihowdima1337@gmail.com>
2025-05-17 11:11:24 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pyautogui as pg
+import mss
+import keyboard
+
+i = 1
+w = 90
+top = 340      
+left = 670                  
+h = 60
+
+mon1 = {'top': top, 'left': left, 'width': w, 'height':h}
+restart_coords = (478, 478)
+
+sct = mss.mss()
+pg.click(restart_coords)
+
+while True:
+    img = np.array(sct.grab(mon1))
+
+    if img.mean() < 247.5:
+        pg.press('space')
+    if w < 600:
+        i+=1
+        if i == 100:
+            w+=4              
+            mon1 = {'top': top, 'left': left, 'width': w, 'height':h}
+            i = 1
+           
+    # cv2.imshow('Cactus Detection', img)
+    # cv2.waitKey(1)
+    if keyboard.is_pressed('q'):
+        break
+
+
+# cv2.destroyAllWindows()
