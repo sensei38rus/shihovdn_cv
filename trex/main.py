b6c91e6f5bae8ffcbce5889ec84f11c5e4b43faf
shihow dmitri <shihowdima1337@gmail.com>
2025-05-17 15:39:02 +0800

    main.py

--- trex/main.py
+++ trex/main.py
@@ -1,35 +1,43 @@
 import numpy as np
+import cv2
+from mss import mss
+from PIL import Image
 import pyautogui as pg
-import mss
 import keyboard
+from time import sleep, time
 
-i = 1
-w = 90
-top = 340      
-left = 670                  
-h = 60
 
-mon1 = {'top': top, 'left': left, 'width': w, 'height':h}
-restart_coords = (478, 478)
-
-sct = mss.mss()
-pg.click(restart_coords)
-
+mon1 = {'top': 210, 'left': 565, 'width': 765, 'height': 200}
+sct1 = mss()
+x2 = 215   
+ 
+start_time = time()
+x2_increment_interval = 10               
+x2_increment_value = 5                                   
+ 
 while True:
-    img = np.array(sct.grab(mon1))
-
-    if img.mean() < 247.5:
-        pg.press('space')
-    if w < 600:
-        i+=1
-        if i == 100:
-            w+=4              
-            mon1 = {'top': top, 'left': left, 'width': w, 'height':h}
-            i = 1
-           
+    
+    current_time = time() 
+    elapsed_time = current_time - start_time
+     
+    if elapsed_time >= x2_increment_interval:
+        x2 += x2_increment_value
+        start_time = current_time 
+      
    
+    sct_img = sct1.grab(mon1)
+    img_pil = Image.frombytes('RGB', sct_img.size, sct_img.rgb)
+    img_arry1 = np.array(img_pil)
+
+    cactus = img_arry1[145:180, 110:int(x2)]
+   
+   
+    if cactus.mean() < 246.2: 
+      pg.press('space')
+
+
+    
     if keyboard.is_pressed('q'):
         break
-
 
 
